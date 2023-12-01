4fc418baa13e67b92a7766909531091a00544c24
Artem <artem.kravchuk.2010@mail.ru>
2023-12-01 13:12:53 +0300

    add close conection

--- main.py
+++ main.py
@@ -10,7 +10,6 @@ user = build('tasks', 'v1', credentials=key)
 # Получение списка всех задач
 results = user.tasks().list(tasklist='@default').execute()
 tasks = results.get('items', [])
-
 # Перебор всех задач и установка срока выполнения, если дата не указана
 for task in tasks:
     task_id = task['id']
@@ -21,3 +20,4 @@ for task in tasks:
         due = (datetime.datetime.now() + datetime.timedelta(hours=1)).isoformat()
         user.tasks().update(tasklist='@default', task=task_id, body={'due': due}).execute()
         print(f'Срок выполнения задачи "{title}" установлен на {due}')
+user.close()
