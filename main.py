48c25533c439a42dfa3fd46c5b0bb707e0b309f5
Artem <artem.kravchuk.2010@mail.ru>
2023-12-01 12:40:25 +0300

    add key

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import datetime
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# Загрузка учетных данных из файла ключа службы Google API
+key = service_account.Credentials.from_service_account_file('key.json')
+# Создание экземпляра API-клиента
+user = build('tasks', 'v1', credentials=key)
 
+# Получение списка всех задач
+results = user.tasks().list(tasklist='@default').execute()
+tasks = results.get('items', [])
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Перебор всех задач и установка срока выполнения, если дата не указана
+for task in tasks:
+    task_id = task['id']
+    title = task['title']
+    due = task.get('due')
+    if not due:
+        # Установка срока выполнения через час от текущего времени
+        due = (datetime.datetime.now() + datetime.timedelta(hours=1)).isoformat()
+        user.tasks().update(tasklist='@default', task=task_id, body={'due': due}).execute()
+        print(f'Срок выполнения задачи "{title}" установлен на {due}')
